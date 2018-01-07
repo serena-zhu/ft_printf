d0403cfba0625b9cca43bd6b5d961425e70ffcf5
Yan jun Zhu <yazhu@e1z2r9p39.42.us.org>
2018-01-06 22:10:49 -0800

    changes for norm

--- convert_p.c
+++ convert_p.c
@@ -6,7 +6,7 @@
 /*   By: yazhu <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/02 20:42:14 by yazhu             #+#    #+#             */
-/*   Updated: 2018/01/06 22:07:04 by yazhu            ###   ########.fr       */
+/*   Updated: 2018/01/06 22:10:35 by yazhu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,10 @@ void	convert_p(t_format *format, va_list ap, int *count)
 	*count += (ft_digits(nbr, 16) + 2);
 	format->min_w -= (ft_digits(nbr, 16) + 2);
 	left_adjusted = ft_haschar(format->flag, '-');
-	while (!left_adjusted && format->min_wd-- > 0 && ++(*count))
+	while (!left_adjusted && format->min_w-- > 0 && ++(*count))
 		ft_putchar(' ');
 	ft_putstr("0x");
 	ft_putnbr_base(nbr, 16, 0);
-	while (left_adjusted && format->min_wd-- > 0 && ++(*count))
+	while (left_adjusted && format->min_w-- > 0 && ++(*count))
 		ft_putchar(' ');
 }
